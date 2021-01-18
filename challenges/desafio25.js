d4a72840473e3df05746ac2fa9a685838866604c
Alvaro Neto <alvaroshn@gmail.com>
2021-01-18 16:13:10 -0300

    Challenge 25 [0]

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -1,2 +1,3 @@
 // 25 - Remova todos os voos da empresa AZUL em que a quantidade de combustível
 //  seja menor do que 400. Informe a quantidade de documentos removidos
+db.voos.deleteMany({ litrosCombustivel: { $lt: 400 }, "empresa.nome": "AZUL" });
