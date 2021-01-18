4c139873a78d7b2aa49153467f069c6afbd84cbb
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-01-18 20:00:26 -0300

    big commit 1-26

---
+++ challenges/desafio22.js
@@ -0,0 +1,1 @@
+db.voos.findOne({ "aeroportoOrigem.sigla": "SBGR", "aeroportoDestino.sigla": "KJFK", $or: [{ "empresa.nome": { $eq: "DELTA AIRLINES" } }, { "empresa.nome": { $eq: "AMERICAN AIRLINES" } }] }, { _id: 0, vooId: 1 });
