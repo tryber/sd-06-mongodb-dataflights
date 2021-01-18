84e708bf32f04838f5020b40f05012c62a5830ed
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-01-18 18:57:22 -0300

    desafio 23

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,1 +1,5 @@
-db.voos.findOne({ $and : [{ litrosCombustivel: { $lt: 1000 } }, { litrosCombustivel: { $exists: true } }] }, { _id: 0, vooId: 1, litrosCombustivel: 1 });
+db.voos.findOne({
+  $and: [
+    { litrosCombustivel: { $lt: 1000 } },
+    { litrosCombustivel: { $exists: true } }
+  ] }, { _id: 0, vooId: 1, litrosCombustivel: 1 });
