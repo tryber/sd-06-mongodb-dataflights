67d78753288c7e4d355519d7eed3040e1737dbc0
luizmacmillan <luizmacmillan@gmail.com>
2021-01-18 17:51:15 -0300

    Correçoes de Linter

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,1 +1,4 @@
-db.voos.findOne({ $and: [{ litrosCombustivel: { $lt: 1000 } }, { litrosCombustivel: { $exists: 1 } }] }, { _id: 0, vooId: 1, litrosCombustivel: 1 });
+db.voos.findOne(
+  { $and: [{ litrosCombustivel: { $lt: 1000 } }, { litrosCombustivel: { $exists: 1 } }] },
+  { _id: 0, vooId: 1, litrosCombustivel: 1 },
+);
