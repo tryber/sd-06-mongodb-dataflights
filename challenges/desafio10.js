675dd3c21c3be0292e01160400896b4e2eb3ece0
Jean Machado <jeanvictor.arq@gmail.com>
2021-01-18 19:02:00 -0300

    desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,11 +2,13 @@ db.voos.find(
   { "empresa.nome": {
     $eq: "GOL" },
   ano: { $eq: 2017 } },
-  { vooId: 1,
+  {
+    _id: 0,
+    vooId: 1,
     empresa: { nome: 1 },
-    aeroportoOrigem:
-{ nome: 1 },
+    aeroportoOrigem: { nome: 1 },
     aeroportoDestino: { nome: 1 },
     mes: 1,
-    ano: 1 },
+    ano: 1,
+  },
 ).limit(10);
