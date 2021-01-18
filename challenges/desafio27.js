85ed883dd1e502f80f08bfd3eeb3550d0437e429
danieleperse <danieleperse3@gmail.com>
2021-01-18 19:28:49 -0300

    correçao 27 e 28

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,2 +1,2 @@
 db.resumoVoos.insertOne({ empresa: "PASSAREDO", totalVoosDomesticos: db.voos.find({ natureza: "Doméstica", "empresa.nome": "PASSAREDO" }).count() });
-db.resumoVoos.findOne({ empresa: "PASSAREDO" }, { empresa: 1, totalVoosDomesticos: 1 });
+db.resumoVoos.findOne({ empresa: "PASSAREDO" }, { empresa: 1, totalVoosDomesticos: 1, _id: 0 });
