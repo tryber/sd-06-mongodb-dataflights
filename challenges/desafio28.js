da298feddc37a9da429e71049b01f33ea5fceff5
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-01-18 19:18:46 -0300

    :sparkles: Desafio 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,3 @@
+db.resumoVoos.insertOne({ empresa: "LATAM AIRLINES BRASIL", totalVoosDomesticos: 20026 });
+
+db.resumoVoos.find({ empresa: "LATAM AIRLINES BRASIL" }, { empresa: true, totalVoosDomesticos: true, _id: false });
