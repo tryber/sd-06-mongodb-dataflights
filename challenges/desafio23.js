f616c925366a468666139090e48ddbd7074e12a0
Ana Flávia <anaflvia@gmail.com>
2021-01-18 16:17:57 -0300

    lint desafio 23 corrigido

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,1 +1,3 @@
-db.voos.find({ $and: [{ litrosCombustivel: { $lte: 1000 } }, { litrosCombustivel: { $exists: true } }] }, { _id: 0, vooId: 1, litrosCombustivel: 1 }).limit(1);
+db.voos.find({ $and:
+  [{ litrosCombustivel: { $lte: 1000 } }, { litrosCombustivel: { $exists: true } }]
+}, { _id: 0, vooId: 1, litrosCombustivel: 1 }).limit(1);
