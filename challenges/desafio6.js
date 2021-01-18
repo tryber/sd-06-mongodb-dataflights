6470ed20bd84e07d527389df34aaa0bbd2c633b9
Jean Machado <jeanvictor.arq@gmail.com>
2021-01-18 18:05:30 -0300

    desafio 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,1 +1,1 @@
-db.voos.find({ vooId: { $eq: 756807 } }, { empresa: { sigla: 1, nome: 1 }, passageiros: 1 });
+db.voos.find({ vooId: { $eq: 756807 } }, { empresa: { sigla: 1, nome: 1 }, passageiros: 1, _id: 0 });
