0e037adf7c196de9fdb9cb65397577dab5207b6b
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2021-01-18 17:18:40 -0300

    Challenges 15 to 28 | FIX 2

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,7 +1,7 @@
 db.voos.findOne({ $and:
   [{ litrosCombustivel:
-    { $not: { $gt: 1000 } }
+    { $not: { $gt: 1000 } },
   }, {
-    litrosCombustivel: { $exists: 1 }
-  }]
+    litrosCombustivel: { $exists: 1 },
+  }],
 }, { _id: 0, vooId: 1, litrosCombustivel: 1 });
