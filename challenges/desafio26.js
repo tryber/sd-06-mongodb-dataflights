c00177ce9036086f2a569b48b48d0bea3f59659a
luizmacmillan <luizmacmillan@gmail.com>
2021-01-18 18:10:26 -0300

    Correçao de Linter.

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,1 +1,1 @@
-db.voos.deleteMany({ $and: [{ "empresa.nome": { $eq: "GOL" } }, { $and: [{ "passageiros.pagos": { $gte: 5 } }, { "passageiros.pagos": { $lte: 10 } }] }] } );
+db.voos.deleteMany({ $and: [{ "empresa.nome": { $eq: "GOL" } }, { $and: [{ "passageiros.pagos": { $gte: 5 } }, { "passageiros.pagos": { $lte: 10 } }] }] });
