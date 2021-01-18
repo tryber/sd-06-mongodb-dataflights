6c7b50857683de173eca27fa33ff6a674d781232
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-01-18 17:31:29 -0300

    Solução desafio 25

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,2 +1,1 @@
-db.voos.findOne({ litrosCombustivel: { $lte: 1000, $exists: true } },
-  { vooId: 1, litrosCombustivel: 1, _id: 0 });
+db.voos.findOne({ litrosCombustivel: { $lte: 1000 } }, { vooId: 1, litrosCombustivel: 1, _id: 0 });
