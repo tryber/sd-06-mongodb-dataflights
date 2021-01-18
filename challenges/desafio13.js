8b62606bdb525c0f16089b8af27949d3dff5705b
isabellavjs <isabellavjs@gmail.com>
2021-01-18 16:54:44 -0300

    Corrige 13 e 14

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,1 +1,1 @@
-db.voos.find({ "aeroportoDestino.continente": { $nin: ["EUROPA", "ÁSIA", "OCEANIA"] } });
+db.voos.find({ "aeroportoDestino.continente": { $nin: ["EUROPA", "ÁSIA", "OCEANIA"] } }).count();
