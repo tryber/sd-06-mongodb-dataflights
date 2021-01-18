428d0488c938ceda323f2c1a07135298f040e8f2
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-18 18:13:13 -0300

    :bug: corrigindo o 24 - excluind id

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -2,4 +2,4 @@ db.voos.findOne({ $nor: [
   { litrosCombustivel: { $gt: 600 } },
   { "empresa.nome": { $in: ["GOL", "AZUL"] } },
   { litrosCombustivel: { $exists: 0 } },
-] }, { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 });
+] }, { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 });
