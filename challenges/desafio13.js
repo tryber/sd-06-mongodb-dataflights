802851f76203a10824a1bd97b74eb9f4f61b2bce
Leandro <leandro.feitoza8018@gmail.com>
2021-01-18 17:48:23 -0300

    desafio 14

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,5 +1,3 @@
-// 13 - Retorne os documentos em que o campo aeroportoDestino.continente não seja igual a EUROPA, ÁSIA e OCEANIA
-
 db.voos.count({
   $and: [
     { "aeroportoDestino.continente": { $ne: "EUROPA" } },
