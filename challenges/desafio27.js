eeb9047f6799b3e7c9e6405bbd98c91d7b011aa3
Micaeli Teixeira <luime6@gmail.com>
2021-01-18 18:35:33 -0300

    feat: add desafio27.js

---
+++ challenges/desafio27.js
@@ -0,0 +1,3 @@
+const quantidadeVoos = db.voos.count({ "empresa.nome": "PASSAREDO", natureza: "Doméstica" });
+db.resumoVoos.insertOne({ empresa: "PASSAREDO", totalVoosDomesticos: quantidadeVoos });
+db.resumoVoos.find({ empresa: "PASSAREDO" }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
