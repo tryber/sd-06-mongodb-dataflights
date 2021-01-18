9664818889c843fbf692e580875527ca45b32757
EliasCarvalho20 <eliascarvalho01@hotmail.com>
2021-01-18 18:46:26 -0300

    All done

---
+++ challenges/desafio24.js
@@ -0,0 +1,5 @@
+db.voos.findOne({ $and: [
+  { litrosCombustivel: { $exists: true } },
+  { litrosCombustivel: { $lte: 600 } },
+  { "empresa.nome": { $not: { $eq: ["GOL", "AZUL"] } } }] },
+{ _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 });
---
+++ challenges/desafio27.js
@@ -0,0 +1,3 @@
+const result = db.voos.find({ "empresa.nome": "PASSAREDO", natureza: "Doméstica" }).count();
+db.resumoVoos.insertOne({ empresa: "PASSAREDO", totalVoosDomesticos: result });
+db.resumoVoos.find({ empresa: "PASSAREDO" }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
