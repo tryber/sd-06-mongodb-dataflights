a17229e2a3a98963892b615e7625ff7ead829adc
LeandroParisi <lepcbelisario@gmail.com>
2021-01-18 17:50:23 -0300

    desfios 27 e 29

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,1 +1,6 @@
-db.voos.find({ $and: [{ "empresa.nome": "PASSAREDO" }, { natureza: "Doméstica" }] }).count();
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos.find({ $and: [{ "empresa.nome": "PASSAREDO" }, { natureza: "Doméstica" }] }).count(),
+});
+
+db.resumoVoos.find({ empresa: "PASSAREDO" }, { empresa: 1, totalVoosDomesticos: 1, _id: 0 }).limit(1);
---
+++ challenges/desafio28.js
@@ -0,0 +1,6 @@
+db.resumoVoos.insertOne({
+  empresa: "LATAM AIRLINES BRASIL",
+  totalVoosDomesticos: db.voos.find({ $and: [{ "empresa.nome": "LATAM AIRLINES BRASIL" }, { natureza: "Doméstica" }] }).count(),
+});
+
+db.resumoVoos.find({ empresa: "LATAM AIRLINES BRASIL" }, { empresa: 1, totalVoosDomesticos: 1, _id: 0 });
