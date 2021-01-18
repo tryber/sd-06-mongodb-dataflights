76baca5f64305f82c428ee3c9400802cfb3a5545
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-01-18 17:58:49 -0300

    :sparkles: Desafio 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,13 @@
+db.voos.find(
+  { $and:
+    [
+      { "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] }},
+      { "aeroportoOrigem.sigla": "SBGR" },
+      { "aeroportoDestino.sigla": "KJFK" },
+    ],
+  },
+  {
+    vooId: true,
+    _id: false,
+  },
+).limit(1);
