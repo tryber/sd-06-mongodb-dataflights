d4568a2639d49b878f74d6f29766af2db66b2d1c
luizmacmillan <luizmacmillan@gmail.com>
2021-01-18 18:28:05 -0300

    Desafio 28.

---
+++ challenges/desafio28.js
@@ -0,0 +1,3 @@
+db.resumoVoos.insertOne({ empresa: "LATAM AIRLINES BRASIL", totalVoosDomesticos: db.voos.find({ $and: [{ "empresa.nome": { $eq: "LATAM AIRLINES BRASIL" } }, { natureza: { $eq: "Doméstica" } }] }).count() });
+
+db.resumoVoos.findOne({ empresa: { $eq: "LATAM AIRLINES BRASIL" } }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
