23b8f17d7abf7b0bec4ee11a39b4ef8b8d14272d
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-01-18 19:20:35 -0300

    Requisite 22 - push 01

---
+++ challenges/desafio22.js
@@ -0,0 +1,2 @@
+/* eslint-disable quotes */
+db.voos.findOne({ $and: [{ $or: [{ "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }] }, { "aeroportoOrigem.sigla": "SBGR" }, { "aeroportoDestino.sigla": "KJFK" }] }, { _id: 0, vooId: 1 });
