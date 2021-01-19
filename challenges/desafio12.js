a5f87ad7508e93bb00d4c46d4bffd109b3870a7c
rafMewtwo <ponciano.raf@gmail.com>
2021-01-19 19:26:51 -0300

    desafio12.3

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,1 +1,1 @@
-db.voos.find({ "aeroportoDestino.pais": { $or: ["BRASIL", "ARGENTINA", "CHILE"] } }).count();
+db.voos.find({ "aeroportoDestino.pais": { $in: ["BRASIL", "ARGENTINA", "CHILE"] } }).count();
