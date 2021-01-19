0ea5e826023b13dab3deb34420fe58edabae71a0
William <williamjog@hotmail.com>
2021-01-19 01:22:55 -0300

    10º requisito - typo

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,13 +1,1 @@
-db.voos.find({
-  $and: [
-    { "empresa.nome": "GOL" },
-    { ano: 2017 },
-  ],
-}, {
-  vooId: 1,
-  "empresa.nome": 1,
-  "aeroportoOrigem.nome": 1,
-  "aeroPortoDestino.nome": 1,
-  mes: 1,
-  ano: 1,
-  _id: 0 }).limit(10);
+db.voos.find({ ano: 2017, "empresa.nome": "GOL" }, { _id: 0, vooId: 1, "empresa.nome": 1, "aeroportoOrigem.nome": 1, "aeroportoDestino.nome": 1, mes: 1, ano: 1 }).limit(10);
