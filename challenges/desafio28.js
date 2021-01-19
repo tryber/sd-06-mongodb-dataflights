6092a0aa7b144c070622db5ce05c0f3c5672fbfd
Tiago Berwanger <berwangertiago@gmail.com>
2021-01-19 00:44:27 -0300

    ajusting desafios 27 e 28

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,2 +1,2 @@
-db.resumoVoos.insertOne({ empresa: "LATAM AIRLINES BRASIL" }, { totalVoosDomesticos: db.voos.count({ "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" }) });
+db.resumoVoos.insertOne({ empresa: "LATAM AIRLINES BRASIL", totalVoosDomesticos: db.voos.count({ "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" }) });
 db.resumoVoos.findOne({ empresa: "LATAM AIRLINES BRASIL" }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
