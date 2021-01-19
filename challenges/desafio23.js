6f9d30c7fdb21ecfc9bd9b87c2cb85f1cc9bb186
pa-volpin <paulo.volpin@gmail.com>
2021-01-18 22:15:18 -0300

    Corrections to hide the id on outputs

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,2 +1,2 @@
 db.voos.findOne({ $and: [{ litrosCombustivel: { $lte: 1000 } },
-  { litrosCombustivel: { $exists: 1 } }] }, { vooId: 1, litrosCombustivel: 1 });
+  { litrosCombustivel: { $exists: 1 } }] }, { vooId: 1, litrosCombustivel: 1, _id: 0 });
