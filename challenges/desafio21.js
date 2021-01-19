fbb7834b8e6d8941db8543cdbc8f4856fdfdec1a
Júlia Pôrto <juportob@gmail.com>
2021-01-19 15:40:39 -0300

    Desafio 21 - fix code

--- challenges/desafio21.js
+++ challenges/desafio21.js
@@ -1,1 +1,1 @@
-db.voos.find({ litrosCombustivel: { $gte: 1000 } }, { _id: 0, vooId: 1, mes: 1, ano: 1 }).limit(1);
+db.voos.findOne({ litrosCombustivel: { $gt: 1000 } }, { vooId: 1, _id: 0 }).limit(1);
