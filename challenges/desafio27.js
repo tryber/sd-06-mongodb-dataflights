79e2476355781e87e7df532b1ed9514568dd5556
Tiago Berwanger <berwangertiago@gmail.com>
2021-01-19 00:25:42 -0300

    Desafios 27 e 28

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,2 +1,2 @@
-db.resumoVoos.insertMany([{ empresa: "PASSAREDO" }, { totalVoosDomesticos: db.voos.count({ "empresa.nome": "PASSAREDO", natureza: { $eq: "Doméstica" } }) }]);
+db.resumoVoos.insertOne([{ empresa: "PASSAREDO" }, { totalVoosDomesticos: db.voos.count({ "empresa.nome": "PASSAREDO", natureza: { $eq: "Doméstica" } }) }]);
 db.resumoVoos.find({ empresa: "PASSAREDO" }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,2 +1,2 @@
-db.resumoVoos.insertMany([{ empresa: "LATAM AIRLINES BRASIL" }, { totalVoosDomesticos: db.voos.count({ "empresa.nome": "LATAM AIRLINES BRASIL", natureza: { $eq: "Doméstica" } }) }]);
+db.resumoVoos.insertOne([{ empresa: "LATAM AIRLINES BRASIL" }, { totalVoosDomesticos: db.voos.count({ "empresa.nome": "LATAM AIRLINES BRASIL", natureza: { $eq: "Doméstica" } }) }]);
 db.resumoVoos.find({ empresa: "LATAM AIRLINES BRASIL" }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
