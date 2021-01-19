9413316744ccdaf8e5d0a8d3111e018d08fcb8a7
Jean Machado <jeanvictor.arq@gmail.com>
2021-01-18 21:03:21 -0300

    desafio 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,7 @@
+db.voos.find(
+  {
+    "empresa.nome": { $in: ["DELTA", "AIRLINES", "AMERICAN AIRLINES"] },
+    "aeroportoOrigem.sigla": { $eq: "SBGR" },
+    "aeroportoDestino.sigla": { $eq: "KJFK" } }, { _id: 0, vooId: 1,
+  },
+).limit(1);
