3c07ada4baa3b0201844602e8e93341bc8497a8d
rafMewtwo <ponciano.raf@gmail.com>
2021-01-19 18:53:52 -0300

    desafio11.2

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,3 +1,13 @@
+// db.voos.find({ "empresa.nome": "GOL" , ano: 2017 },
+// {
+//   vooId: 1,
+//   "empresa.nome": 1,
+//   "aeroportoOrigem.nome": 1,
+//   "aeroportoDestino.nome": 1,
+//   mes: 1,
+//   ano: 1,
+//   _id: 0,
+// }).limit(10);
 // precisa de um AND para os parametros de busca
 db.voos.find(
   {
