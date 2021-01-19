4a9e88e4cd61cfb931de413368abce723e1a24a3
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-01-18 22:25:42 -0300

    feat: flys of latam

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,11 +1,11 @@
-db.resumoVoos.insertMany(
-  [{
+db.resumoVoos.insertMany([
+  {
     empresa: "PASSAREDO",
     totalVoosDomesticos: db.voos.count({
       natureza: "Doméstica",
       "empresa.nome": "PASSAREDO",
     }),
-  }],
-);
+  },
+]);
 
-db.resumoVoos.findOne({}, { _id: 0 });
+db.resumoVoos.findOne({ empresa: "PASSAREDO" }, { _id: 0 });
