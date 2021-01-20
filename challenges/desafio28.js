6033e79b440b67a7aec23b8acde52852e09db36d
ricardorosa-dev <ricardorosa@gmail.com>
2021-01-19 22:12:20 -0300

    desafio 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,14 @@
+const myQuery = db.voos.find(
+  { "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" },
+).count();
+
+db.resumoVoos.insertOne(
+  { 
+    empresa: "LATAM AIRLINES BRASIL",
+    totalVoosDomesticos: myQuery,
+  },
+);
+
+db.resumoVoos.find(
+  { empresa: "LATAM AIRLINES BRASIL" },
+);
