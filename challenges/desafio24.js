4010ed903acbf9f24600755ccf31ce843671088e
kissyla <kissylamottapereira@gmail.com>
2021-01-20 18:44:40 -0300

    desafio 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,10 @@
+db.voos.find(
+  { $and:
+    [
+      { litrosCombustivel: { $lte: 600 }},
+      { litrosCombustivel: { $exists: true }},
+      { empresa: { $ne: "GOL"}},
+      { empresa: { $ne: "AZUL"}}
+    ]},
+      { vooId: true, "empresa.nome": true,litrosCombustivel: true, _id: false })
+      .limit(1);
