dd0f660db56493485e096866ee85b0d8f42040ce
Pedro Duarte <ikbenpedro@gmail.com>
2021-01-20 17:17:34 -0300

    desafio23

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,1 +1,2 @@
-db.voos.find({ litrosCombustivel: { $lte: 1000 }, litrosCombustivel: { $exists: true } }, { vooId: 1, litrosCombustivel: 1, _id: 0 }).limit(1);
+db.voos.find({
+  litrosCombustivel: { $lte: 1000 } }, { vooId: 1, litrosCombustivel: 1, _id: 0 }).limit(1);
