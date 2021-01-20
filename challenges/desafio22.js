8c9b5182347512a7ca1dc1cb2fbb6b6074b16db8
rafMewtwo <ponciano.raf@gmail.com>
2021-01-20 15:26:59 -0300

    desafio22.6

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,4 +1,10 @@
-// db.voos.findOne({ $or: [{ "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }], "aeroporto.origem": "SBGR", "aeroporto.destino": "KJFK" }, { vooId: true, _id: false });
+// db.voos.findOne(
+//   { $or: [
+//     { "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }
+//   ], "aeroporto.origem": "SBGR", "aeroporto.destino": "KJFK" 
+// }, 
+// { vooId: true, _id: false }
+// );
 db.voos.findOne({
   $and: [
     {
