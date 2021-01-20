911b8d42311ad2d07095281891149988405b9fa3
rafMewtwo <ponciano.raf@gmail.com>
2021-01-20 07:00:34 -0300

    desafio25.4

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -1,1 +1,2 @@
-db.voos.deleteMany({ $and: [{ "empresa.nome": "AZUL" }, { litrosCombustivel: { $lt: 400 } }] }).count();
+// nao precisa de count para retornar quantos foram removidos
+db.voos.deleteMany({ $and: [{ "empresa.nome": "AZUL" }, { litrosCombustivel: { $lt: 400 } }] });
