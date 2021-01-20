d1430f90f59f41f22ca25dfbe31e3ba39314af96
kissyla <kissylamottapereira@gmail.com>
2021-01-20 18:51:42 -0300

    codeclimate desafios 23 e 24

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,1 +1,9 @@
-db.voos.find({ $or: [{ empresa: "DELTA AIRLINES" }, { empresa: "AMERICAN AIRLINES" }], "aeroportoOrigem.sigla": "SBGR", "aeroportoDestino.sigla": "KJFK" }, { vooId: true, _id: false }).limit(1);
+db.voos.find({
+  $or:
+  [
+    { empresa: "DELTA AIRLINES" }, 
+    { empresa: "AMERICAN AIRLINES" }
+  ],
+  "aeroportoOrigem.sigla": "SBGR",
+  "aeroportoDestino.sigla": "KJFK" },
+  { vooId: true, _id: false }).limit(1);
