5304b1c5d98a87c49f35dccb1827cac8a6e93edb
Paulo <paulocheloni@gmail.com>
2021-01-21 14:47:09 -0300

    adding req 27, req 28

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,3 +1,3 @@
-let newerQuery = db.voos.find({ "empresa.nome": { $eq: "LATAM AIRLINES BRASIL" } , natureza: { $eq:"Doméstica"} }).count();
+const newerQuery = db.voos.find({ "empresa.nome": { $eq: "LATAM AIRLINES BRASIL" } , natureza: { $eq: "Doméstica" } }).count();
 db.resumoVoos.insert({ empresa: "LATAM AIRLINES BRASIL", totalVoosDomesticos: newerQuery });
 db.resumoVoos.findOne({ empresa: "LATAM AIRLINES BRASIL" }, { _id: 0, totalVoosDomesticos: 1, empresa: 1 });
