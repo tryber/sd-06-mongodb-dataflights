65adb4ff33c6cdcffca62ebf17849d848e248485
rafMewtwo <ponciano.raf@gmail.com>
2021-01-20 21:06:07 -0300

    desafio23.8

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,3 +1,3 @@
-// db.voos.findOne(
-//   { litrosCombustivel: { $lte: 1000 } }, { _id: false, vooId: true, litrosCombustivel: true },
-// );
+db.voos.findOne(
+  { litrosCombustivel: { $lte: 1000 } }, { _id: false, vooId: true, litrosCombustivel: true },
+);
