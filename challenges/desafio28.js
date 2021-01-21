c8b1277df55d070d93c1765771a327c41dd1217d
rafMewtwo <ponciano.raf@gmail.com>
2021-01-21 19:36:03 -0300

    desafio28

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,1 +1,13 @@
-db.resumevoos.insert()
+db.resumevoos.insertOne({
+  empresa: "LATAM AIRLINES BRASIL",
+  totalVoosDomesticos: 20026
+});
+
+db.resumevoos.findOne(
+  {
+  empresa : "LATAM AIRLINES BRASIL"
+  },
+  {
+    empresa: 1, totalVoosDomesticos: 1, _id: 0,
+  }
+);
