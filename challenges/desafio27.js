c22b30b31aa5a17f0023df339f943b3617528f6c
Paulo <paulocheloni@gmail.com>
2021-01-21 13:30:53 -0300

    adding req 27

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,2 +1,6 @@
-db.voos.aggregate([{ $match: { natureza: "Doméstica", "empresa.nome": "LATAM AIRLINES BRASIL" } }, { $out: "resumoVoos" }]);
-db.resumoVoos.find({}).count();
+
+const quant = db.voos.find({ "empresa.nome": { $eq: "PASSAREDO" }, natureza: { $eq: "Doméstica" } }).count();
+print(quant);
+db.resumoVoos.insertOne({ empresa: "PASSAREDO" , totalVoosDomesticos: quant });
+db.resumoVoos.find({ "empresa": "PASSAREDO" }, { _id: 0 });
+
