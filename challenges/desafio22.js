7c1e25bbbd4c5603fa85c0ab88d35cd8912f1e70
Alessandra <itsalematos@hotmail.com>
2021-01-22 17:25:37 -0400

    fix: 22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,1 +1,1 @@
-db.voos.findOne({ $and: [{ $or: [{ "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }] }, { $or: [{ "aeroportoOrigem.sigla": "SBGR" }, { "aeroportoOrigem.sigla": "KJFK" }] }] }, { vooId: 1, _id: 0 });
+db.voos.findOne({ $and: [{ $or: [{ "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }] }, { $and: [{ "aeroportoOrigem.sigla": "SBGR" }, { "aeroportoDestino.sigla": "KJFK" }] }] }, { vooId: 1, _id: 0 });
