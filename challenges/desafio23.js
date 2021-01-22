f22ada2dcdd9ee9a4bec31752c5ae5422f0b5d17
VinicinBraga <vinicius@bragamatta.com.br>
2021-01-22 14:23:34 -0300

    Desafios 24 ao 28

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,6 +1,6 @@
 db.voos.findOne({
   $and: [
     { litrosCombustivel: { $lte: 1000 } },
-    { litrosCombustivel: { $exists: 1 } },
+    { litrosCombustivel: { $exists: true } },
   ],
 }, { vooId: 1, litrosCombustivel: 1, _id: 0 });
