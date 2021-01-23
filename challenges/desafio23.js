c51d62301fb27b674d8d94810aeee52eba0447c5
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2021-01-23 13:10:50 -0300

    lint ate 23

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,1 +1,2 @@
-db.voos.findOne( { $and: [ { litrosCombustivel: { $lt: 1000 }, litrosCombustivel: { $exists: true } } ] }, { vooId: 1, litrosCombustivel: 1, _id: 0 } );
+db.voos.findOne({ $and: [{ litrosCombustivel: { $lt: 1001 } },
+  { litrosCombustivel: { $exists: true } }] }, { vooId: 1, litrosCombustivel: 1, _id: 0 });
