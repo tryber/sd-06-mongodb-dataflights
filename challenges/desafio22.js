83faaacc9528bfdb31b5bcacbb2b234b27390271
pa-volpin <paulo.volpin@gmail.com>
2021-01-23 16:25:56 -0300

    Correction on task 22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,1 +1,1 @@
-db.voos.findOne({ $and: [{ $or: [{ "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }] }, { "aeroportoOrigem.sigla": "SBGR" }, { "aeroportoOrigem.sigla": "KJFK" }] }, { _id: 0, vooId: 1 });
+db.voos.findOne({ $and: [{ $or: [{ "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }] }, { "aeroportoOrigem.sigla": "SBGR" }, { "aeroportoDestino.sigla": "KJFK" }] }, { _id: 0, vooId: 1 });
