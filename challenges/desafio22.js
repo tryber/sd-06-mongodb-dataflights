ff8e3fbeba57a762e22b15c4f5450c609270c5ee
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2021-01-23 13:13:10 -0300

    lint 22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,2 +1,4 @@
 db.voos.findOne({ "empresa.nome": { $in: ["AMERICAN AIRLINES", "DELTA AIRLINES"] },
-"aeroportoOrigem.sigla": "SBGR", "aeroportoDestino.sigla": "KJFK" }, { vooId: 1, _id: 0 });
+  "aeroportoOrigem.sigla": "SBGR",
+  "aeroportoDestino.sigla": "KJFK" },
+{ vooId: 1, _id: 0 });
