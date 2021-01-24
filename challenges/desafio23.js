13ae5a7ef4642a3b1b585348dfd4f2098462a8ff
Pedro Eduardo <pedroedu77@gmail.com>
2021-01-23 21:39:23 -0300

    desafio 23 lint

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,1 +1,6 @@
-db.voos.findOne({ $and: [{ litrosCombustivel: { $lt: 1000 } }, { litrosCombustivel: { $exists: 1 } },] }, { vooId: 1, litrosCombustivel: 1, _id: 0 });
+db.voos.findOne({
+  $and: [
+  { litrosCombustivel: { $lt: 1000 } },
+  { litrosCombustivel: { $exists: 1 } },
+  ]
+}, { vooId: 1, litrosCombustivel: 1, _id: 0 });
