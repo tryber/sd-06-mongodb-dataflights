3e53f048b846b93f469e9d2752f7a2ef20dcfa28
PAULO LINS <paulolins100@gmail.com>
2021-01-25 12:03:19 -0300

    chalenge19

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,4 +1,4 @@
 db.voos.findOne(
-    { litrosCombustivel: { $exists: true } },
-    { _id: false, vooId: true },
-  );
+  { litrosCombustivel: { $exists: true } },
+  { _id: false, vooId: true },
+);
