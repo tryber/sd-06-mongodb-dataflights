3848d9bfc0bf87efe41c4a5b8990aad031b4a51d
Marcelo Pessini <marceloppessini@gmail.com>
2021-01-27 11:42:46 -0300

    desafio

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,2 +1,3 @@
-db.voos.find({ litrosCombustivel: { $lte: 1000, $exists: 1 } },
-  { vooId: 1, litrosCombustivel: 1, _id: 0 }).limit(1);
+db.voos.find({ $and: [{ litrosCombustivel: { $lte: 1000 } },
+  { litrosCombustivel: { $exists: 1 } }] },
+{ vooId: 1, litrosCombustivel: 1, _id: 0 }).limit(1);
