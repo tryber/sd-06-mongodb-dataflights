894799f9c72293d71ea195628b1c0227c357c2ab
Gui-Alucard <guilhermeanv7822@gmail.com>
2021-02-15 21:14:12 -0300

    :sparkles: Desafio 24
    not exists e or na mesma busca

---
+++ challenges/desafio24.js
@@ -0,0 +1,1 @@
+db.voos.findOne({ litrosCombustivel: { $not: { $gt: 600 }, $exists: true }, $or: [{ "empresa.nome": { $ne: "GOL" } }, { "empresa.nome": { $ne: "AZUL" } }] }, { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 });
