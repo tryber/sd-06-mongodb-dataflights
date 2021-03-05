c6e969be9e930dffa0417688516f95e171e5c38a
fomes <contato.felipeluna@gmail.com>
2021-03-05 13:42:01 -0300

    finish project

---
+++ challenges/desafio24.js
@@ -0,0 +1,7 @@
+db.voos.findOne(
+  { $and: [
+    { litrosCombustivel: { $lte: 600 } },
+    { "empresa.nome": { $nin: ["GOL", "AZUL"] } },
+  ] },
+  { _id: false, vooId: true, litrosCombustivel: true, "empresa.nome": true },
+);
